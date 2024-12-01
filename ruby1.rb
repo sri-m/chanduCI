ccd67f39be7c76cc1f13a895333b83ec4024475a
sri-m <srinivaspkl@gmail.com>
2024-12-01 12:33:32 +0530

    Waiting for Success

--- ruby1.rb
+++ ruby1.rb
@@ -1,7 +1,7 @@
 # script.rb
-puts "Looking for final output"
-puts "Uncheck for following in circleCI"
+#puts "Looking for final output"
+#puts "Uncheck for following in circleCI"
 
 puts 'I changed following option + circleCI generated repo in github + I changed yml file code in this repo'
-
+puts 'Waiting for success'
 #ba2521eb-7447-4c0f-9f27-d23110b492d0  - circleCI pipeLine ID
