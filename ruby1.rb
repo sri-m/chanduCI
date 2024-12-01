68770036a9689223a21f41a0a5d6f785eb89d081
sri-m <srinivaspkl@gmail.com>
2024-12-01 11:40:32 +0530

    added for CircleCI

--- ruby1.rb
+++ ruby1.rb
@@ -1,6 +1,9 @@
 # script.rb
 puts "Hello, this is your Ruby script running in CI!";
 puts "testing 2nd time";
+puts 'checking for CIRCLE CI THIS LINE';
+puts 'checks second time in Cicle CI THIS LINE'
+
 #comment added
 #second
  
