5dbafaf1d0280182d0a0288128317d77c29b2e2c
sri-m <srinivaspkl@gmail.com>
2024-12-01 12:23:32 +0530

    today circleCI file

--- ruby1.rb
+++ ruby1.rb
@@ -2,4 +2,6 @@
 puts "Looking for final output"
 puts "Uncheck for following in circleCI"
 
+puts 'I changed following option + circleCI generated repo in github + I changed yml file code in this repo'
+
 #ba2521eb-7447-4c0f-9f27-d23110b492d0  - circleCI pipeLine ID
