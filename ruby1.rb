c23e1aaac07d656bf75796ad705c1aa64932a943
sri-m <srinivaspkl@gmail.com>
2024-12-01 13:03:05 +0530

    Waiting for Success2

--- ruby1.rb
+++ ruby1.rb
@@ -3,5 +3,6 @@
 #puts "Uncheck for following in circleCI"
 
 puts 'I changed following option + circleCI generated repo in github + I changed yml file code in this repo'
-puts "Waiting for success"
+puts "Deleted sub repo in hub"
+
 #ba2521eb-7447-4c0f-9f27-d23110b492d0  - circleCI pipeLine ID
