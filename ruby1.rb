e787f2b709d893967b3f8c95f1dc4f5cac08e6f6
sri-m <srinivaspkl@gmail.com>
2024-12-01 13:08:36 +0530

    Waiting for Success3

--- ruby1.rb
+++ ruby1.rb
@@ -4,5 +4,6 @@
 
 puts 'I changed following option + circleCI generated repo in github + I changed yml file code in this repo'
 puts "Deleted sub repo in hub"
+print 'new srini project setup'
 
 #ba2521eb-7447-4c0f-9f27-d23110b492d0  - circleCI pipeLine ID
