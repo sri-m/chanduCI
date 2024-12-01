4dab0152c83fe60d958b7580637829009abcf7f9
sri-m <srinivaspkl@gmail.com>
2024-12-01 17:26:53 +0530

    last trail Success7

--- ruby1.rb
+++ ruby1.rb
@@ -1,9 +1,9 @@
 # script.rb
 #puts "Looking for final output"
 #puts "Uncheck for following in circleCI"
-
+puts 'cimg/ruby'
 puts 'I changed following option + circleCI generated repo in github + I changed yml file code in this repo'
 puts "Deleted sub repo in hub"
-print 'last try 333 for Docker image'
+print 'last try 333 for Docker image cimg/ruby'
 
 #ba2521eb-7447-4c0f-9f27-d23110b492d0  - circleCI pipeLine ID
